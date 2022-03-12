ae6307fa06b73ffd20d15e9964251c5c35adfee8
Shweta Pandey <7spandey@gmail.com>
2022-03-12 21:36:21 +0530

    updated setting for swagger api doc access

--- users/settings.py
+++ users/settings.py
@@ -155,3 +155,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SWAGGER_SETTINGS = {
+   'USE_SESSION_AUTH': False
+}
