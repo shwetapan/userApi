19a85bdaef0021d17096cf0547a00d12106923c5
Shweta Pandey <7spandey@gmail.com>
2022-03-12 21:49:16 +0530

    making debug to true for doc

--- users/settings.py
+++ users/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -156,6 +156,3 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-SWAGGER_SETTINGS = {
-   'USE_SESSION_AUTH': False
-}
